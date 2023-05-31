d1516ba2ff8c19540f3de865892bfaa395b37ea9
Fab15ger <103307589+Fab15ger@users.noreply.github.com>
2023-05-31 13:40:48 -0300

    Update __init__.py

--- comunidadeimpressionadora/__init__.py
+++ comunidadeimpressionadora/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 import os
+import sqlalchemy
 
 
 app = Flask(__name__)
@@ -18,5 +19,17 @@ login_manager = LoginManager(app)
 login_manager.login_view = 'login_criarconta'
 login_manager.login_message = 'Por favor faça login ou crie uma conta para continuar'
 login_manager.login_message_category = 'alert-info'
+
+from comunidadeimpressionadora import models
+engine = sqlalchemy.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+inspector = sqlalchemy.inspect(engine)
+if not inspector.has_table('usuario'):
+    with app.app_context():
+        database.drop_all()
+        database.create_all()
+        print('Base de Dados criado')
+else:
+    print('Base de Dados já existente.')
+
 
 from comunidadeimpressionadora import routes
