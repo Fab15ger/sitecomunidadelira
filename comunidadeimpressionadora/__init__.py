7522126a5731a4b7f0dd56b6d6243743ee59726b
Fab15ger <103307589+Fab15ger@users.noreply.github.com>
2023-05-31 20:51:52 -0300

    Update __init__.py

--- comunidadeimpressionadora/__init__.py
+++ comunidadeimpressionadora/__init__.py
@@ -12,7 +12,9 @@ app.config['SECRET_KEY'] = '71959ebc89ab53fc286e22ea9fd99888'
 if os.getenv("DATABASE_URL"):
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
 else:
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
+    #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'PGPASSWORD=uniza0kj00OeDTYfB7Jg psql -h containers-us-west-126.railway.app -U postgres -p 5667 -d railway'
+    
 
 database = SQLAlchemy(app)
 bcript = Bcrypt(app)
